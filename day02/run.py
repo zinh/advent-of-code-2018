82965c0970dfc043b69db178378cff3fb9ff4b6a
Vinh Nguyen <xuanvinhct@gmail.com>
2018-12-02 14:52:25 +0900

    Add day02

---
+++ day02/run.py
@@ -0,0 +1,24 @@
+def letter_count(input):
+    h = {}
+    for c in input:
+        if c in h:
+            h[c] += 1
+        else:
+            h[c] = 1
+    return h
+
+f = open('input')
+count2 = 0
+count3 = 0
+
+for line in f:
+    count = letter_count(line)
+    for k, v in count.items():
+        if v == 2:
+            count2 += 1
+            break
+    for k, v in count.items():
+        if v == 3:
+            count3 += 1
+            break
+print(count2 * count3)

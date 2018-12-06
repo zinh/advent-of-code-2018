2a47960177dbe954a6aa799ec8416d5468760178
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-12-06 17:20:54 +0900

    Day 6

--- day06/part01.py
+++ day06/part01.py
@@ -7,26 +7,26 @@ def read_file(file_name):
     return points
 
 def process():
-    points = read_file('input')
+    points = read_file('input1')
     max_col = max(points, key=lambda x: x[0])[0]
     max_row = max(points, key=lambda x: x[1])[1]
-    grids = [[(0, None) for col in range(0, max_col)] for row in range(0, max_row)]
+    grids = [[(None, None) for col in range(0, max_col + 1)] for row in range(0, max_row + 1)]
     for idx, point in enumerate(points):
-        update_grids(grids, idx, [point], 1, max_col, max_row)
+        update_grids(grids, idx, [point], 0, max_col, max_row)
+        break
 
 def update_grids(grids, label, points, distance, max_col, max_row):
+    if distance == 20:
+        return grids
     for point in points:
         col, row = point
-        print(row, col)
-        print(max_col, max_row)
         d, l = grids[row][col]
         if l is None:
             grids[row][col] = (distance, label)
-            update_grids(grids, label, adjecent_points(point, max_col, max_row), distance + 1, max_col, max_row)
         elif l != label:
             if distance < d:
                 grids[row][col] = (distance, label)
-                update_grids(grids, label, adjecent_points(point, max_col, max_row), distance + 1, max_col, max_row)
+        update_grids(grids, label, adjecent_points(point, max_col, max_row), distance + 1, max_col, max_row)
     return grids
 
 def adjecent_points(point, max_col, max_row):
@@ -34,7 +34,10 @@ def adjecent_points(point, max_col, max_row):
     return [(col + i, row + j) for i in [0, 1, -1] for j in [0, 1, -1] if valid(col, i, row, j, max_col, max_row)]
 
 def valid(col, i, row, j, max_col, max_row):
-    next_col, next_row = col + i, row + i
-    return (i != 1 or j != 1) and next_col >= 0 and next_col < max_col and next_row >= 0 and next_row < max_row
+    next_col, next_row = col + i, row + j
+    return (i != 0 or j != 0) and next_col >= 0 and next_col <= max_col and next_row >= 0 and next_row <= max_row
 
+def print_grid(grids):
+    for row in grids:
+        print(row)
 process()

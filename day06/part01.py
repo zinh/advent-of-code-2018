dd96412cbcf83965292579838329b76ba1463746
Vinh Nguyen <xuanvinhct@gmail.com>
2018-12-07 01:39:02 +0900

    Day 6, part 1 solution

--- day06/part01.py
+++ day06/part01.py
@@ -7,16 +7,28 @@ def read_file(file_name):
     return points
 
 def process():
-    points = read_file('input1')
+    points = read_file('input')
     max_col = max(points, key=lambda x: x[0])[0]
     max_row = max(points, key=lambda x: x[1])[1]
+    #print(max_col, max_row)
     grids = [[(None, None) for col in range(0, max_col + 1)] for row in range(0, max_row + 1)]
     for idx, point in enumerate(points):
+        print(idx)
         update_grids(grids, idx, [point], 0, max_col, max_row)
-        break
+    all_labels = {cell[1] for row in grids for cell in row}
+    border_points = grids[0] + grids[max_row] + [row[0] for row in grids] + [row[max_col] for row in grids]
+    border_labels = {point[1] for point in border_points}
+    finite_labels = all_labels - border_labels
+    m = max([count_label(grids, label) for label in finite_labels])
+    print(m)
+
+def count_label(grids, label):
+    return sum([1 for row in grids for cell in row if cell[1] == label])
 
 def update_grids(grids, label, points, distance, max_col, max_row):
-    if distance == 20:
+    #print_grid(grids)
+    neightbors = []
+    if not points:
         return grids
     for point in points:
         col, row = point
@@ -26,18 +38,28 @@ def update_grids(grids, label, points, distance, max_col, max_row):
         elif l != label:
             if distance < d:
                 grids[row][col] = (distance, label)
-        update_grids(grids, label, adjecent_points(point, max_col, max_row), distance + 1, max_col, max_row)
-    return grids
+        for p in adjecent_points(point, max_col, max_row):
+            c, r = p
+            d, l = grids[r][c]
+            if (l is None or (l != label and d > distance + 1 )) and not duplicated(neightbors, p):
+                neightbors.append(p)
+    update_grids(grids, label, neightbors, distance + 1, max_col, max_row)
 
 def adjecent_points(point, max_col, max_row):
     col, row = point
-    return [(col + i, row + j) for i in [0, 1, -1] for j in [0, 1, -1] if valid(col, i, row, j, max_col, max_row)]
+    return [(next_col, next_row) for next_col, next_row in [(col, row + 1), (col, row - 1), (col + 1, row), (col - 1, row)] if valid(next_col, next_row, max_col, max_row)]
 
-def valid(col, i, row, j, max_col, max_row):
-    next_col, next_row = col + i, row + j
-    return (i != 0 or j != 0) and next_col >= 0 and next_col <= max_col and next_row >= 0 and next_row <= max_row
+def duplicated(points, point):
+    for p in points:
+        if p[0] == point[0] and p[1] == point[1]:
+            return True
+    return False
+
+def valid(col, row, max_col, max_row):
+    return col >= 0 and col <= max_col and row >= 0 and row <= max_row
 
 def print_grid(grids):
+    print("========")
     for row in grids:
-        print(row)
+        print("".join([str(cell[1]) if cell[1] is not None else 'x' for cell in row]))
 process()

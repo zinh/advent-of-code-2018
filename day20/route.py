f0134e67c1486b41addc46ed17dde4f64d1cea45
Vinh Nguyen <xuanvinhct@gmail.com>
2018-12-21 01:49:11 +0900

    Update routes

--- day20/route.py
+++ day20/route.py
@@ -9,7 +9,7 @@ def parse(file_name):
     return routes
 
 def main():
-    route = parse('input3')
+    route = parse('input2')
     m = {}
     stack = []
     pos = 0
@@ -44,18 +44,27 @@ def main():
             current_coord = (row, col)
         pos += 1
     #print(stack)
-    draw_map(m)
     #print('m = ', m)
-    count_doors(m)
+    distances = count_doors(m)
+    draw_map(m, distances)
     return m
 
-def draw_map(m):
+def draw_map(m, distances):
     rows = list(m.keys())
     cols = [col_no for row_no, row in m.items() for col_no, _ in row.items() ]
     min_row, max_row = min(rows), max(rows)
     min_col, max_col = min(cols), max(cols)
-    arr = [['x' if r == 0 and c == 0 else m.get(r, {}).get(c, '#') for c in range(min_col - 1, max_col + 2)] for r in range(min_row - 1, max_row + 2)]
+    arr = [[print_cell(r, c, m, distances) for c in range(min_col - 1, max_col + 2)] for r in range(min_row - 1, max_row + 2)]
     print('\n'.join([''.join(a) for a in arr]))
+
+def print_cell(row, col, m, distances):
+    if row == 0 and col == 0:
+        return 'x'
+    cell = m.get(row, {}).get(col, '#')
+    if cell == '.':
+        #return '{:02d}'.format(distances[row][col])
+        return str(distances[row][col])
+    return cell
 
 def count_doors(m):
     distances = {0: {0: 0}}
@@ -64,20 +73,22 @@ def count_doors(m):
         for col_no, col in row.items():
             distances[row_no][col_no] = None
     update_distance(m, distances, (0, 0), 0)
-    print('distances = ', distances)
+    return distances
 
 def update_distance(m, distances, pos, current_distance):
+    print(pos)
     r, c = pos
     neighbors = [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]
-    #print(pos)
     for (row, col) in neighbors:
         cell = m.get(row, {}).get(col)
-        #import pdb; pdb.set_trace()
+        #if pos == (2, -2):
+        #    import pdb; pdb.set_trace()
         if cell == '|' or cell == '-':
             if distances[row][col] is None:
+                distances[row][col] = current_distance
                 update_distance(m, distances, (row, col), current_distance + 1)
         elif cell == '.':
-            if distances[row][col] is None:# or distances[row][col] < current_distance:
+            if distances[row][col] is None or distances[row][col] < current_distance:
                 distances[row][col] = current_distance
                 update_distance(m, distances, (row, col), current_distance)
 

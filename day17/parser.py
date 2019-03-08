526efef91782a62e20b8fea3c8b3c6679190aea7
Vinh Nguyen <vinhnx@zigexn.co.jp>
2019-03-08 19:10:13 +0900

    update day 17

--- day17/parser.py
+++ day17/parser.py
@@ -1,6 +1,17 @@
 def parse(file_name):
+    input_data = []
     with open(file_name) as f:
         for line in f:
-            pass
+            first, second = line.strip().split(', ')
+            input_data.append([split_expr(first), split_expr(second)])
+    return input_data
 
-parse('input')
+def split_expr(s):
+    label, numbers = s.split('=')
+    if '..' in numbers:
+        start, end = numbers.split('..')
+        return label, list(range(int(start), int(end) + 1))
+    else:
+        return label, [int(numbers)]
+
+print(parse('input'))
